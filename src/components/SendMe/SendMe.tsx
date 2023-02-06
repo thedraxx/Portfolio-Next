d810b0c01fa35f4434029914206abf2d9b325596
thedraxx <thedraxxgames@gmail.com>
2023-02-05 22:44:16 -0300

    added some types in sendme

--- src/components/SendMe/SendMe.tsx
+++ src/components/SendMe/SendMe.tsx
@@ -32,7 +32,7 @@ const SendMe = () => {
 
                 <div className="md:flex md:items-center mb-6">
                     <div className="md:w-1/3">
-                        <label className="block text-white font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+                        <label className="block text-white font-bold md:text-right mb-1 md:mb-0 pr-4">
                             Your Email:
                         </label>
                     </div>
@@ -51,7 +51,7 @@ const SendMe = () => {
                 </div>
                 <div className="md:flex md:items-center mb-6">
                     <div className="md:w-1/3">
-                        <label className="block text-white font-bold md:text-right mb-1 md:mb-0 pr-4" for="inline-password">
+                        <label className="block text-white font-bold md:text-right mb-1 md:mb-0 pr-4">
                             Your Message:
                         </label>
                     </div>
